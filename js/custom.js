5129c5e25dce1f6ed63b3c70beb72e8491af80ec
teddy <teddahissiho@gmail.com>
2021-02-18 15:12:15 +0000

    new addditions

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-let handlePaymentPeriod = ({monthlyPrice, yearlyPrice}) => {
+const handlePaymentPeriod = ({monthlyPrice, yearlyPrice}) => {
     let toogler = document.getElementById("toogler")
     let priceAmount = document.getElementById("price-amount");
     let isMonthly = toogler.className === "toogle-month"
@@ -11,5 +11,15 @@ let handlePaymentPeriod = ({monthlyPrice, yearlyPrice}) => {
         priceAmount.childNodes[1].data = monthlyPrice
         priceAmount.lastChild.innerText = " /year"
 
+    }
+}
+
+const handleMoveBar = (post) => {
+    let bar = document.getElementsByClassName("selection-bar");
+    bar = bar[0];
+    if(post === 1) {
+        bar.style.marginLeft = "0%"
+    }else {
+        bar.style.marginLeft = "60%"
     }
 }
