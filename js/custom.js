9a18bcde76b635fdaacd10f6f649990c7e893293
teddy <teddahissiho@gmail.com>
2021-02-23 07:44:48 +0000

    new additions

--- js/custom.js
+++ js/custom.js
@@ -5,11 +5,25 @@ const handlePaymentPeriod = ({monthlyPrice, yearlyPrice}) => {
     if(isMonthly) {
         toogler.className = "toogle-year"
         priceAmount.childNodes[1].data = yearlyPrice
-        priceAmount.lastChild.innerText  = " /month"
+        priceAmount.lastChild.innerText  = " /an"
     } else {
         toogler.className = "toogle-month"
         priceAmount.childNodes[1].data = monthlyPrice
-        priceAmount.lastChild.innerText = " /year"
+        priceAmount.lastChild.innerText = " /mois"
+
+    }
+}
+
+const handlePaymentPeriodWithPost = ({monthlyPrice, yearlyPrice}) => {
+    let toogler = document.getElementById("toogler")
+    let priceAmount = document.getElementById("price-amount");
+    let isMonthly = toogler.className === "toogle-month"
+    if(isMonthly) {
+        priceAmount.childNodes[1].data = monthlyPrice
+        priceAmount.lastChild.innerText = " /mois"
+    } else {
+        priceAmount.childNodes[1].data = yearlyPrice
+        priceAmount.lastChild.innerText  = " /an"
 
     }
 }
@@ -26,13 +40,40 @@ const handleMoveBar = (post) => {
     secondText = secondText[1];
 
     bar = bar[0];
-    if(post === 1) {
-        bar.style.marginLeft = "0%"
-    }else {
-        bar.style.marginLeft = "60%"
+
+    const switchFirst = ({state}) => {
+        if(state === "off") {
+            firstPost.style.color = "black";
+            firstText.style.color = "black"
+        } else if (state === "on") {
+            firstPost.style.color = "white";
+            firstText.style.color = "white";
+            handlePaymentPeriodWithPost({monthlyPrice: "10.000", yearlyPrice: "90.000"})
+
+        }
     }
 
-    const switchFirst = () => {
-        
+    const switchSecond = ({state}) => {
+        if(state === "off") {
+            secondPost.style.color = "black";
+            secondText.style.color = "black"
+        } else if (state === "on") {
+            secondPost.style.color = "white";
+            secondText.style.color = "white";
+            handlePaymentPeriodWithPost({monthlyPrice: "35.000", yearlyPrice: "320.000"})
+
+        }
     }
+
+    if(post === 1) {
+        bar.style.marginLeft = "0%"
+        switchFirst({state: "on"});
+        switchSecond({state: "off"});
+    }else {
+        bar.style.marginLeft = "60%"
+        switchSecond({state: "on"});
+        switchFirst({state: "off"});
+    }
+
+
 }
