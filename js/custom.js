12d0e410250b6940aaf2573338f71757702ae4f6
teddy <teddahissiho@gmail.com>
2021-04-07 19:21:09 +0000

    update

--- js/custom.js
+++ js/custom.js
@@ -234,3 +234,38 @@ const openSection = (sectionId) => {
       isOpen = true;
     }
 }
+
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn1 = document.getElementById("myBtn1");
+
+// Get the button that opens the modal
+const btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
